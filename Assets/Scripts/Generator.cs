3d03f7ab3a95d2617591d843999216299f9a9d4c
Nataly Mironova <knightalli@mail.ru>
2024-10-18 11:10:19 +0500

    add hiding information

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -25,23 +26,48 @@ public class Generator : MonoBehaviour
         _catReasons = allParameters[0].reasons;
         _catNatures = allParameters[0].natures;
         _catImages = allParameters[0].images;
+
+        AddNewCat();
+    }
+
+    public void AddNewCat()
+    {
+        int randomImages = Random.Range(0, _catImages.Count);
+        //_image = _catImages[randomImages];
+        CleanCat();
     }
 
     public void GenerateCat()
     {
         int randomNames = Random.Range(0, _catNames.Count);
         int randomReasons = Random.Range(0, _catReasons.Count);
-        int randomNatures = Random.Range(0, _catNatures.Count);
-        int randomImages = Random.Range (0, _catImages.Count);
+        int randomNatures = Random.Range(0, _catNatures.Count);        
         int randomAge = Random.Range(0, 20);
         int randomMouseCount = Random.Range(0, 5000);
 
         _name.text = _catNames[randomNames];
         _reason.text = _catReasons[randomReasons];
-        _nature.text = _catNatures[randomNatures];
-        //_image = _catImages[randomImages];
+        _nature.text = _catNatures[randomNatures];        
         _age.text = randomAge.ToString();
         _mouseCount.text = randomMouseCount.ToString();
+    }
+
+    private void CleanCat()
+    {
+        string empty = "...";
+        _name.text = empty;
+        _reason.text = empty;
+        _nature.text = empty;
+        _age.text = empty;
+        _mouseCount.text = empty;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GenerateCat();
+        }
     }
 
 }
