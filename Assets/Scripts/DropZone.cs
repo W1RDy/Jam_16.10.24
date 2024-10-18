b34e0f6dae0d7d9b8688967597e83a11e3cf02ec
Nataly Mironova <knightalli@mail.ru>
2024-10-18 19:10:36 +0500

    add image and seal

--- Assets/Scripts/DropZone.cs
+++ Assets/Scripts/DropZone.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class DropZone : MonoBehaviour, IDropHandler
 {
     [SerializeField] Generator _generator;
+    [SerializeField] Sprite[] _seals;
+    [SerializeField] Image _sealPlace;
 
     public void OnDrop(PointerEventData eventData)
     {
@@ -13,7 +16,8 @@ public class DropZone : MonoBehaviour, IDropHandler
 
         if (droppedObject.TryGetComponent(out LiveSeal liveSeal))
         {
-            _generator.AddNewCat();        
+            AddSeal(0);
+            Invoke("DeleteSeal", 5f);              
         }
 
         if (droppedObject.TryGetComponent(out DeadSeal deadSeal))
@@ -21,8 +25,21 @@ public class DropZone : MonoBehaviour, IDropHandler
             int deaths = PlayerPrefs.GetInt("deaths");
             deaths += 1;
             PlayerPrefs.SetInt("deaths", deaths);
-            _generator.AddNewCat();
-            
+
+            AddSeal(1);
+            Invoke("DeleteSeal", 3f);
         }
+    }
+
+    private void DeleteSeal()
+    {
+        _sealPlace.gameObject.SetActive(false);
+        _generator.AddNewCat();
+    }
+
+    private void AddSeal(int number)
+    {
+        _sealPlace.gameObject.SetActive(true);
+        _sealPlace.sprite = _seals[number];
     }
 }
